7a2c6096581bf5d94bef4579f3b5a458eb8cf831
Matricarin <matricarin@gmail.com>
2023-04-02 07:27:56 +0500

    Отрисовка окна и границ поля.

--- SnakeGame/Pixel.cs
+++ SnakeGame/Pixel.cs
@@ -13,16 +13,24 @@ namespace SnakeGame
         public int X { get; }
         public int Y { get; }
         public ConsoleColor Color { get; }
-        public Pixel(int x, int y, ConsoleColor color)
+        public int PixelSize { get; }
+        public Pixel(int x, int y, ConsoleColor color, int pixelSize = 3)
         {
             X = x;
             Y = y;
             Color = color;
+            PixelSize = pixelSize;
         }  
         public void Draw()
         {
-            SetCursorPosition(X, Y);
-            Write(PixelChar);
+            for(int x = 0; x < PixelSize; x++)
+            {
+                for(int y = 0; y < PixelSize; y++)
+                {
+                    SetCursorPosition(X * PixelSize + x, Y * PixelSize + y);
+                    Write(PixelChar);
+                }
+            }
         }
         public void Clear()
         {
--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -1,10 +1,44 @@
 ﻿using static System.Console;
+using SnakeGame;
 
+#region Constants
 const int MapWidth = 30;
 const int MapHeight = 20;
 
-SetWindowSize(MapWidth, MapHeight);
-SetBufferSize(MapWidth, MapHeight);
+const int ScreenWidth = MapWidth * 3;
+const int ScreenHeight = MapHeight * 3;
+
+const ConsoleColor BorderColor = ConsoleColor.Gray;
+#endregion
+
+
+#region WindowSettings
+SetWindowSize(ScreenWidth, ScreenHeight);
+SetBufferSize(ScreenWidth, ScreenHeight);
 CursorVisible = false;
 
+#endregion
+
+#region MainLogic
+DrawBorder();
 ReadKey();
+#endregion
+
+
+#region ConsoleMethods
+static void DrawBorder()
+{
+    for(int i = 0; i < MapWidth;  i++)
+    {
+        new Pixel(i, 0, BorderColor).Draw();
+        new Pixel(i, MapHeight - 1, BorderColor).Draw();
+    }
+
+    for(int i = 0; i < MapHeight; i++)
+    {
+        new Pixel(0, i, BorderColor).Draw();
+        new Pixel(MapWidth -1 , i, BorderColor).Draw();
+    }
+}
+
+#endregion

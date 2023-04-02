439ca0f8ba54f594a5d7a51b97c3bbf0dc4efffc
Matricarin <matricarin@gmail.com>
2023-04-02 06:46:33 +0500

    Add project files.

---
+++ SnakeGame/Pixel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SnakeGame
+{
+    internal struct Pixel
+    {
+        private const char PixelChar = '█';
+        public int X { get; }
+        public int Y { get; }
+        public ConsoleColor Color { get; }
+        public Pixel(int x, int y, ConsoleColor color)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }  
+        public void Draw()
+        {
+            SetCursorPosition(X, Y);
+            Write(PixelChar);
+        }
+        public void Clear()
+        {
+            SetCursorPosition(X, Y);
+            Write(' ');
+        }
+    }
+}
---
+++ SnakeGame/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+
+const int MapWidth = 30;
+const int MapHeight = 20;
+
+SetWindowSize(MapWidth, MapHeight);
+SetBufferSize(MapWidth, MapHeight);
+CursorVisible = false;
+
+ReadKey();
